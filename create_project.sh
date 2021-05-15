3a1cee171a6e4f39f788597427290e4140f3b588
peTheProgrammer <peTheProgrammer@gmail.com>
2021-05-15 14:22:22 +0200

    Reverted last change which broke the recursion in replace_variable_in_files.

--- create_project.sh
+++ create_project.sh
@@ -44,7 +44,7 @@ else
 	
 	replace_variables_in_files()
 	{
-		for file in $Project_Name/*; do
+		for file in $1/*; do
 			if [ -f $file ]; then
 				sed -i "s/\${Project_Kind}/$Project_Kind/g" $file
 				sed -i "s/\${Project_Name_Exe}/$Project_Name_Exe/g" $file
